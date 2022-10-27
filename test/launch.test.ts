6264e6f0e5a00d3e643cc3974dbd879d24802c20
Oleksii Demchenko <demchenko.alexey.v@gmail.com>
2022-10-27 17:35:08 +0300

    Add basic teardown

--- test/launch.test.ts
+++ test/launch.test.ts
@@ -2,12 +2,18 @@ import {chromium} from "playwright";
 
 describe('Launch browser', () => {
     test('Open SwagLabs', async () => {
+        // setup
         const browser = await chromium.launch({
             headless: false
         });
         const context = await browser.newContext();
         const page = await context.newPage();
+
+        // action
         await page.goto('https://www.saucedemo.com/');
+
+        // teardown
+        await context.close();
         await browser.close();
     })
 })
