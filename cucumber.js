6dae186d9db7bfcb8fdfd599120ba15e06ac00fc
Oleksii Demchenko <demchenko.alexey.v@gmail.com>
2022-11-04 18:38:19 +0200

    Add report generation

--- cucumber.js
+++ cucumber.js
@@ -2,6 +2,7 @@ let options = [
     '--require-module ts-node/register',
     '--require ./tests/e2e/steps/*steps.ts',
     '--format progress',
+    '--format json:./reports/cucumber_report.json',
 ].join(' ');
 
 let run_features = [
---
+++ htmlReportGenerator.js
@@ -0,0 +1,19 @@
+const reporter = require('cucumber-html-reporter');
+var date = new Date();
+var currentDate = date.getDate() + '_' + (date.getMonth());
+
+var options = {
+    brandTitle: "demo",
+    theme: 'bootstrap',
+    jsonFile: 'reports/cucumber_report.json',
+    output: 'reports/cucumber_report_' + currentDate + '.html',
+    screenshotsDirectory: './screenshots/',
+    storeScreenshots: true,
+    reportSuiteAsScenarios: true,
+    launchReport: true,
+    metadata: {
+        "version": "1.1.1"
+    }
+};
+
+reporter.generate(options);
