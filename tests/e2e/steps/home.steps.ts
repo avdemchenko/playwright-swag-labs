b39962498d5ffc6ee17e526c6e3f9617072234a7
Oleksii Demchenko <demchenko.alexey.v@gmail.com>
2022-11-04 18:05:27 +0200

    Remove redundant assertions

--- tests/e2e/steps/home.steps.ts
+++ tests/e2e/steps/home.steps.ts
@@ -1,12 +1,9 @@
-import { Given, When, Then } from "@cucumber/cucumber";
-import { page } from "../../../hook";
-import { expect } from "@playwright/test";
+import { Given, Then } from "@cucumber/cucumber";
 import { openPage } from "../modules/common";
 import { verifyPageTitle } from "../modules/assert";
 
 Given('I open {string} page', async (pageName) => {
     await openPage(pageName);
-    await expect(page).toHaveTitle('Swag Labs');
 });
 
 Then('I should see {string} page title', async (pageTitle) => {
