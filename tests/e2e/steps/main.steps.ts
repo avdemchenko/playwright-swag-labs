5c5cb73bccac69bd4f9460a056bfc22b788d1415
Oleksii Demchenko <demchenko.alexey.v@gmail.com>
2022-11-04 10:58:47 +0200

    Rename cucumber hook

--- tests/e2e/steps/main.steps.ts
+++ tests/e2e/steps/main.steps.ts
@@ -1,7 +1,7 @@
 import { Given, When, Then } from "@cucumber/cucumber";
-import { page } from "../../../world";
+import { page } from "../../../hook";
 import { expect } from "@playwright/test";
 
 Given('I am on main page', async () => {
     await expect(page).toHaveTitle('Swag Labs');
-})
+});
